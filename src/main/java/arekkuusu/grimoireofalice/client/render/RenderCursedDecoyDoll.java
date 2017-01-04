dd4cde477ba9fb1f5bddfbc9ebd17db384412f7c
ArekkuusuJerii <edgargcx99@gmail.com>
2017-01-04 02:11:49 -0600

    Updated Doll model

--- src/main/java/arekkuusu/grimoireofalice/client/render/RenderCursedDecoyDoll.java
+++ src/main/java/arekkuusu/grimoireofalice/client/render/RenderCursedDecoyDoll.java
@@ -1,17 +1,16 @@
 package arekkuusu.grimoireofalice.client.render;
 
 import arekkuusu.grimoireofalice.client.ResourceLocations;
-import arekkuusu.grimoireofalice.client.model.ModelCursedDecoyDoll;
+import arekkuusu.grimoireofalice.client.model.ModelDoll;
 import arekkuusu.grimoireofalice.common.entity.EntityCursedDecoyDoll;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
-
 public class RenderCursedDecoyDoll extends Render<EntityCursedDecoyDoll> {
 
-	private static final ModelCursedDecoyDoll MODEL = new ModelCursedDecoyDoll();
+	private static final ModelDoll MODEL = new ModelDoll();
 
 	public RenderCursedDecoyDoll(RenderManager renderManager) {
 		super(renderManager);
@@ -21,10 +20,9 @@ public class RenderCursedDecoyDoll extends Render<EntityCursedDecoyDoll> {
 	public void doRender(EntityCursedDecoyDoll entity, double x, double y, double z, float entityYaw, float partialTicks) {
 		GlStateManager.pushMatrix();
 		bindEntityTexture(entity);
-		GlStateManager.translate(x, y + 0.7, z);
-		GlStateManager.scale(0.5, 0.5, 0.5);
+		GlStateManager.translate(x - 0.1, y + 0.7, z - 0.1);
 		GlStateManager.rotate(180F, 1.0F, 0.0F, 0.0F);
-		GlStateManager.rotate(entity.rotationYaw, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(entity.rotationYaw - 180, 0.0F, 1.0F, 0.0F);
 		MODEL.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
 		GlStateManager.popMatrix();
 	}
