55cd47528f7dbe2f868480020222a5fb342f4500
ArekkuusuJerii <edgargcx99@gmail.com>
2017-01-04 01:57:49 -0600

    Updated Block Onbashira Item

--- src/main/java/arekkuusu/grimoireofalice/common/item/ItemBlockOnbashira.java
+++ src/main/java/arekkuusu/grimoireofalice/common/item/ItemBlockOnbashira.java
@@ -20,29 +20,32 @@ public class ItemBlockOnbashira extends ItemBlock {
 	}
 
 	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		IBlockState iblockstate = worldIn.getBlockState(pos);
-		Block block = iblockstate.getBlock();
-
-		if (!block.isReplaceable(worldIn, pos)) {
-			pos = pos.offset(facing);
-		}
-
-		//TODO: Is the extra canPlaceBlockAt check needed here, and if so, is there a better and easier way to do it?
-		if (stack.stackSize != 0 && playerIn.canPlayerEdit(pos, facing, stack)
-				&& worldIn.canBlockBePlaced(this.block, pos, false, facing, null, stack) && this.block.canPlaceBlockAt(worldIn, pos)) {
-			int i = this.getMetadata(stack.getMetadata());
-			IBlockState iblockstate1 = this.block.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, i, playerIn);
-
-			if (placeBlockAt(stack, playerIn, worldIn, pos, facing, hitX, hitY, hitZ, iblockstate1)) {
-				SoundType soundtype = worldIn.getBlockState(pos).getBlock().getSoundType(worldIn.getBlockState(pos), worldIn, pos, playerIn);
-				worldIn.playSound(playerIn, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-				--stack.stackSize;
-			}
-
-			return EnumActionResult.SUCCESS;
+		if (facing != EnumFacing.UP) {
+			return EnumActionResult.FAIL;
 		}
 		else {
-			return EnumActionResult.FAIL;
+			IBlockState iblockstate = worldIn.getBlockState(pos);
+			Block block = iblockstate.getBlock();
+
+			if (!block.isReplaceable(worldIn, pos)) {
+				pos = pos.offset(facing);
+			}
+
+			if (stack.stackSize != 0 && playerIn.canPlayerEdit(pos, facing, stack)
+					&& worldIn.canBlockBePlaced(this.block, pos, false, facing, null, stack) && this.block.canPlaceBlockAt(worldIn, pos)) {
+				int i = this.getMetadata(stack.getMetadata());
+				IBlockState iblockstate1 = this.block.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, i, playerIn);
+
+				if (placeBlockAt(stack, playerIn, worldIn, pos, facing, hitX, hitY, hitZ, iblockstate1)) {
+					SoundType soundtype = worldIn.getBlockState(pos).getBlock().getSoundType(worldIn.getBlockState(pos), worldIn, pos, playerIn);
+					worldIn.playSound(playerIn, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+					--stack.stackSize;
+				}
+
+				return EnumActionResult.SUCCESS;
+			} else {
+				return EnumActionResult.FAIL;
+			}
 		}
 	}
 }
