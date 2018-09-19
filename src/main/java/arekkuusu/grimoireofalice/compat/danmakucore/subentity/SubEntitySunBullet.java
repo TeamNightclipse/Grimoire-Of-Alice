5d09fa2150c0c826a44e4ed18fb1e9ef70ac361e
ArekkuusuJerii <edgargcx99@gmail.com>
2018-09-19 12:57:52 -0500

    Bouncing bullets

--- src/main/java/arekkuusu/grimoireofalice/compat/danmakucore/subentity/SubEntitySunBullet.java
+++ src/main/java/arekkuusu/grimoireofalice/compat/danmakucore/subentity/SubEntitySunBullet.java
@@ -48,47 +48,43 @@ public class SubEntitySunBullet extends SubEntityType {
 			return DanmakuUpdate.oneUpdate(danmaku, new DanmakuUpdateSignal() {
 				@Override
 				public Option<DanmakuState> process(DanmakuState state) {
-					state = state.copy(
-							state.entity(),
-							state.extra().copy(
-									state.extra().user(),
-									state.extra().source(),
-									state.extra().shot().copy(
-											state.extra().shot().form(),
-											state.extra().shot().renderProperties(),
-											state.extra().shot().edgeColor(),
-											state.extra().shot().coreColor(),
-											state.extra().shot().damage(),
-											state.extra().shot().sizeX(),
-											state.extra().shot().sizeY(),
-											state.extra().shot().sizeZ(),
-											state.extra().shot().delay(),
-											state.extra().shot().end(),
-											state.extra().shot().subEntity()
-									),
-									state.extra().subEntity(),
-									state.extra().movement(),
-									state.extra().rotation()
-							),
-							state.tracking()
-					);
 					EnumFacing facing = raytrace.sideHit;
 					Vector3 vec = state.direction();
+					Vector3 direction;
+					Vector3 multiply;
 					if(facing == EnumFacing.UP || facing == EnumFacing.DOWN) {
-						state.entity().setDirection(new Vector3(vec.getX(), vec.getY() * -1D, vec.getZ()));
-						state.motion().multiply(1, -0.5D, 1);
+						direction = new Vector3(vec.getX(), vec.getY() * -0.8D, vec.getZ());
+						multiply = new Vector3(1, -0.5D, 1);
 					} else {
 						double x = vec.getX();
 						double z = vec.getZ();
 						if(facing == EnumFacing.EAST || facing == EnumFacing.WEST) {
-							state.motion().multiply(-0.5D, 1, 1);
-							x *= -1D;
+							multiply = new Vector3(-0.5D, 1, 1);
+							x *= -0.8D;
 						} else {
-							state.motion().multiply(1, 1, -0.5D);
-							z *= -1D;
+							multiply = new Vector3(1, 1, -0.5D);
+							z *= -0.8D;
 						}
-						state.entity().setDirection(new Vector3(x, vec.getY(), z));
+						direction = new Vector3(x, vec.getY(), z);
 					}
+					state = state.copy(
+							state.entity().copy(
+									state.entity().id(),
+									state.entity().world(),
+									state.entity().ticksExisted(),
+									state.entity().renderBrightness(),
+									state.entity().pos(),
+									state.entity().prevPos(),
+									state.entity().orientation(),
+									state.entity().prevOrientation(),
+									state.entity().motion().multiply(multiply),
+									direction,
+									state.entity().rawBoundingBoxes(),
+									state.entity().rawEncompassingAABB()
+							),
+							state.extra(),
+							state.tracking()
+					);
 					return Option.apply(state);
 				}
 			});
